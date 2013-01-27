44122b1fe0f944993d38da0a30831e9755391ba1
unknown <SteveNat@.(none)>
2013-01-27 01:42:18 +0000

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,85 @@
 pyStatsFC
 =========
 
-python wrapper around the StatsFC API
+Light-weight python wrapper around the StatsFC API. 
+
+Follows conventions from [StatsFC API v2.0 Developer page](https://statsfc.com/developers/ "With a Title") with following exceptions:
+
+*Fixtures* and *Results*
+------------------------
+These are grouped in the **Matches** class. You pass a _type_ argument which can be 'fixtures' or 'results'.
+Date arguments are called *date_from* and *date_to* ('from' being reserved in python) and can be passed as python datetime.date objects or strings in yyyy-mm-dd form.
+
+Quick examples
+--------------
+
+#### Fixtures
+
+    >>> from pyStatsFC import *
+    >>> for reds in Matches('premier-league', 'fixtures', team='Liverpool'):
+    ...     print reds
+    ...
+    status : u'Not started'
+    away_id : 8650
+    date : u'2013-01-30 20:45:00'
+    home : u'Arsenal'
+    away : u'Liverpool'
+    id : 1229350
+    home_id : 9825
+    ------ *** ------
+    .
+    .
+    ...
+
+#### Table
+
+    >>> table = Table('premier-league')
+    >>> for row in table:
+    ...     print row
+    info : u'championsleague'
+    drawn : 2
+    lost : 3
+    for : 57
+    played : 23
+    against : 30
+    team_id : 10260
+    won : 18
+    team : u'Manchester United'
+    position : 1
+    difference : 27
+    points : 56
+    ------ *** ------
+    info : u'championsleague'
+    drawn : 6
+    lost : 2
+    for : 45
+    played : 23
+    against : 19
+    team_id : 8456
+    won : 15
+    team : u'Manchester City'
+    position : 2
+    difference : 26
+    points : 51
+    ------ *** ------
+    info : u'championsleague'
+    drawn : 6
+    lost : 4
+    for : 47
+    played : 23
+    against : 22
+    team_id : 8455
+    won : 13
+    team : u'Chelsea'
+    position : 3
+    difference : 25
+    points : 45
+    ------ *** ------
+    .
+    .
+    ...
+    
+##### Table position method
+
+    >>> print "In tenth position are", Table('premier-league').position(7).team
+    ... In tenth position are Stoke City
---
+++ pyStatsFC.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python2.7
+from operator import itemgetter
+import httplib2, urllib, json, time, string, copy
+from datetime import date, timedelta
+
+__KEY__ = 'free' # obviously chnage this is you have a paid API key
+
+class Struct:
+    ''' Structure object for data items. '''
+    def __init__(self, **entries): 
+        self.__dict__.update(entries)
+    def __repr__(self): return '%s\n------ *** ------' % str('\n'.join('%s : %s' % (k, repr(v)) for (k, v) in self.__dict__.iteritems()))
+    
+class SFCError(Exception):
+    ''' Some branded errors. '''
+    pass
+
+class StatsFC(object):
+    ''' StatsFC base object. '''
+    def __init__(self, competition, data, limit=''):
+        self.base_url = "http://api.statsfc.com/{0}/{1}.json?key={2}&{3}".format(competition, data, __KEY__, urllib.urlencode({'limit':limit}))
+        self.headers = {'Content-type': 'application/json;charset=utf-8', 'Accept-Encoding': 'compress, gzip'}
+        
+    def request(self, additional_params={}, maximimum_retries=10):
+        success = False
+        retries = 0
+        while not success and retries < maximimum_retries:                          
+            http = httplib2.Http()
+            request_url = self.base_url + '&' + urllib.urlencode(additional_params)
+            self.http_headers, response = http.request(request_url, headers=self.headers)
+            if self.http_headers['status'] == '403':
+                try:
+                    retry_after = self.http_headers['retry_after']
+                    time.sleep(retry_after)
+                    retries =+ 1
+                except KeyError :
+                    pass
+            elif self.http_headers['status'] == '200':
+                success = True                                        
+                self.response = json.loads(response)
+                if 'error' in self.response:
+                    raise SFCError(self.response['error'])
+            else:
+                raise SFCError(self.http_headers['status'])
+            
+class Table(StatsFC):
+    ''' Table object '''
+    def __init__(self, competition):
+        super(Table, self).__init__(competition, 'table')
+        self.request()
+        self.rows = [Struct(**data) for data in self.response]
+    
+    def __iter__(self):
+        return iter(self.rows)
+    
+    def __getitem__(self,i):
+        return sorted(self.rows, key=itemgetter('position'))[i]
+    
+    def position(self, pos):
+        ''' allows reference of one-based position in table '''
+        if (pos < 1) or (pos > len(self.rows)):
+            raise SFCError("Position {pos} is out of the range of 1-{num_teams}".format(pos=pos, num_teams=len(self.rows)))
+        else:
+            return self.rows[pos-1]
+        
+class Matches(StatsFC):
+    ''' Fixtures or Results object. 
+    
+    date_from & date_to need to be either datetime.date objects or 'yyyy-mm-dd' strings
+    
+    '''
+    def __init__(self, competition, type, **kwargs):
+        super(Matches, self).__init__(competition, type, limit=kwargs.get('limit',''))     
+        self.from_date = kwargs.get('date_from',date.today())
+        self.to_date = kwargs.get('date_to',date.today()+timedelta(days=365))
+        self.team = kwargs.get('team','')
+        self.request(additional_params={'from':self.from_date, 'to':self.to_date, 'team':self.team})
+        self.items = [Struct(**data) for data in self.response]
+        
+    def __iter__(self):
+        return iter(self.items)      
+
+class Live(StatsFC):
+    ''' Live Matches object. '''
+    def __init__(self, competition, **kwargs):
+        super(Live, self).__init__(competition, 'live', limit=kwargs.get('limit',''))     
+        self.team = kwargs.get('team','')
+        self.request(additional_params={'team':self.team})
+        self.items = [Struct(**data) for data in self.response]
+        
+    def __iter__(self):
+        return iter(self.items)            
+        
+class Form(StatsFC):
+    ''' Form object. '''
+    def __init__(self, competition, **kwargs):
+        super(Form, self).__init__(competition, 'form')     
+        self.request()
+        self.items = [Struct(**data) for data in self.response]
+        
+    def __iter__(self):
+        return iter(self.items)
+
+class TopScorers(StatsFC):
+    ''' Top Scorers object. '''
+    def __init__(self, competition, **kwargs):
+        super(TopScorers, self).__init__(competition, 'top-scorers')     
+        self.request()
+        self.items = [Struct(**data) for data in self.response]
+        
+    def __iter__(self):
+        return iter(self.items)
+
+table = Table('premier-league')
+for row in table:
+    print row
+    
+print "In tenth position are", table.position(10).team
+    
